a53aca01002c7adbd9bc75f7fa1d7deee8310e3d
SaiTejaswani123 <tejaswani.bikkasani@gmail.com>
2024-03-20 23:42:44 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -26,13 +26,3 @@ for i in range(len(data_dict)):
             connection.login(user=my_email, password=password)
             connection.sendmail(from_addr=my_email, to_addrs=data_dict[i]["email"], msg=f"subject: Birthday Wishes\n\n"
                                                                                         f"{birthday_content}")
-
-
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
